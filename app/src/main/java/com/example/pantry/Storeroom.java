4bfaddce1dc87a6c12043b0ed1e673f969f4cedc
Cameron <cjr555@york.ac.uk>
2021-04-27 17:33:28 +0100

    date sorting

--- app/src/main/java/com/example/pantry/Storeroom.java
+++ app/src/main/java/com/example/pantry/Storeroom.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.CountDownTimer;
 import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,8 +22,14 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -35,6 +42,7 @@ public class Storeroom extends Fragment {
     private IngredientAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -86,9 +94,50 @@ public class Storeroom extends Fragment {
         }
     }
 
+    private void sortExpireFirst(){
+        Collections.sort(mIngredientList, new DateSort());
+
+        new CountDownTimer(3000, 100) {
+            public void onTick(long millisUntilFinished) {
+                //necessary method in countdown timer thats called each interval
+            }
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onFinish() {
+                sortExpireLast();
+            }
+        }.start();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void sortExpireLast(){
+        Collections.sort(mIngredientList, new DateSort().reversed());
+        mAdapter.notifyDataSetChanged();
+    }
+
+static class DateSort implements Comparator<IngredientItem>{
+    @Override
+    public int compare(IngredientItem ingredientItem, IngredientItem t1) {
+SimpleDateFormat original = new SimpleDateFormat("dd-MM-yyyy");//original European looking date
+SimpleDateFormat reversed = new SimpleDateFormat("yyyy-MM-dd");//International date format (needed to sort dates properly)
+Date dateIngredient1 = null;//Holds date of first item being compared
+Date dateIngredient2=null;//Holds date of seccond item being compared
+
+try {
+    dateIngredient1=original.parse(ingredientItem.getBestByDate());//Place the original dates in these Date variables
+    dateIngredient2=original.parse(t1.getBestByDate());
+}catch (ParseException e){
+    e.printStackTrace();
+}
+        assert dateIngredient1 != null;
+        assert dateIngredient2 != null;
+        //The dates are reformatted to the reversed SimpleDateFormat before they go back
+        return reversed.format(dateIngredient1).compareTo(reversed.format(dateIngredient2));
+    }
+}
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void buildRecyclerView() {
-        //mIngredientList.sort(Comparator.comparing(o -> mIngredientList.get(0).getBestByDate()));
+        sortExpireFirst();//By default, show user what expires first
         mRecyclerView = view.findViewById(R.id.recyclerview);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(getContext());
